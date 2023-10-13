82b0e9ccb82da2de2c6784aad8b057f14af7dfd2
Mark <Mark@b.com>
2023-10-13 16:01:43 +0800

    fix path plugins

--- src/CUSTOMER/App.vue
+++ src/CUSTOMER/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import layout from './layout/index.vue'
-import { useBus } from '@/CORE/plugin/bus'
-import { onMounted } from 'vue'
+import { useBus } from '@/CORE/plugins/bus'
+import { onMounted, onUnmounted } from 'vue'
 
 const $bus = useBus()
 
+const callback = (str: string) => alert(str)
+
 onMounted(() => {
-  $bus.on('foo', (v) => alert(v))
+  $bus.on('foo', callback)
+})
+
+onUnmounted(() => {
+  $bus.off('foo', callback)
 })
 </script>
 
--- src/CUSTOMER/main.ts
+++ src/CUSTOMER/main.ts
@@ -1,31 +1,30 @@
-import './assets/tailwind.scss'
+import "./assets/tailwind.scss";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import createMeta from '@/CORE/plugin/createMeta'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import createMeta from "@/CORE/plugins/createMeta";
+import { emitter } from "@/CORE/plugins/bus";
+import router from "./router";
+import App from "./App.vue";
 
-import { emitter } from '@/CORE/plugin/bus'
-
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
-    $bus: typeof emitter
-    $sum: (a: number, b: number) => number
-    $cry: (str: string) => void
+    $bus: typeof emitter;
+    $sum: (a: number, b: number) => number;
+    $cry: (str: string) => void;
   }
 }
 
-const app = createApp(App)
-app.config.globalProperties.$sum = (a: number, b: number) => a + b
-app.config.globalProperties.$cry = (str: string) => alert(str)
-app.config.globalProperties.$bus = emitter
+const app = createApp(App);
+app.config.globalProperties.$sum = (a: number, b: number) => a + b;
+app.config.globalProperties.$cry = (str: string) => alert(str);
+app.config.globalProperties.$bus = emitter;
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
 
 /** 在html加入建立日期 */
-document.documentElement.dataset.updated = BUILD_TIME
-createMeta('last-modified', BUILD_TIME)
+document.documentElement.dataset.updated = BUILD_TIME;
+createMeta("last-modified", BUILD_TIME);
