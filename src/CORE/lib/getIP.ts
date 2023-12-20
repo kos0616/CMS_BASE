7c72f9aa7d47e43ba58337dcaff6241f40b4fea9
MARK LIANG <kos0616@gmail.com>
2023-12-20 10:41:10 +0800

    getIP Promise.race => Promise.any

--- src/CORE/lib/getIP.ts
+++ src/CORE/lib/getIP.ts
@@ -1,11 +1,14 @@
-/** 以 Promise.race 取得最快回傳的IP資料 */
+/** 
+ * 以 Promise.any 取得最快回傳的IP資料
+ * 之前用 .race 跑第一的如果翻車會全員坐牢
+ */
 export default async () => {
   /** 這三隻API都是回傳text 因此可以用 text() 方法，若未來有包含到回傳其他格式的API(ex: json())，則需要重新修改 */
   const APIs = ['https://get.geojs.io/v1/ip', 'https://api.ipify.org', 'https://ipinfo.io/ip'];
   const promises = APIs.map((api) => fetch(api).then((response) => response.text()));
 
   try {
-    const result = await Promise.race(promises);
+    const result = await Promise.any(promises);
     return result;
   } catch (error) {
     // 在這裡處理錯誤
